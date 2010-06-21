27c60d851680d7d8bb1a27c62a95bc432579228e
Gaetano Giunta <gg@ez.no>
2010-06-21 10:29:22 +0000

    git-svn-id: http://svn.projects.ez.no/ezonrails@3 2b132ba1-f2ec-4eb1-8ca4-634b1ce61bd4

--- trunk/extension/ezonrails/classes/ezonrailscontroller.php
+++ trunk/extension/ezonrails/classes/ezonrailscontroller.php
@@ -5,8 +5,6 @@
  * @version $Id$
  * @license code licensed under the GNU General Public License v2.0
  * @author G. Giunta
- * @license code licensed under the GNU General Public License v2.0
- * @author G. Giunta
  * @copyright (C) G. Giunta 2010
  */
 

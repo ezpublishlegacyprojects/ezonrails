ffc0634831204243a354019650eb7f618048695e
Gaetano Giunta <gg@ez.no>
2010-06-24 14:46:42 +0000

    - more docs
    git-svn-id: http://svn.projects.ez.no/ezonrails@12 2b132ba1-f2ec-4eb1-8ca4-634b1ce61bd4

--- trunk/extension/ezonrails/controllers/samplecontroller.php
+++ trunk/extension/ezonrails/controllers/samplecontroller.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * A class used to demonstrate how 'controllers' work
  *
  * @version $Id$
  * @license code licensed under the GNU General Public License v2.0
@@ -8,6 +8,10 @@
  * @copyright (C) G. Giunta 2010
  */
 
+/**
+* NB: making this a subclass  of ezOnRailsController is not necessary at all.
+* It is only done here to take advantage of the renderView method
+*/
 class samplecontroller extends ezOnRailsController
 {
 
@@ -18,26 +22,35 @@ class samplecontroller extends ezOnRailsController
     }
 
     /**
-    *  An action using a template to render its output
-    * nb: a php warning will be generated when user goes to /ezonrails/samplecontroller/action1 and omits the param p1
+    * An action using a template to render its output
+    * nb: a php warning will be generated when a user goes to /ezonrails/samplecontroller/action1 and omits the param p1
     */
     function action1( $p1 )
     {
-        return $this->renderView( 'controllers/samplecontroller/action1.tpl', array( 'p1' => $p1 ) );
+        // NB: the template used here is not delivered with this extension!
+        // If you want to use templates to render the output of your methods remember to:
+        // 1. declare your extension as design extension
+        // 2. create the templates you use
+        return $this->renderView( 'design:controllers/samplecontroller/action1.tpl', array( 'FirstParamReceived' => $p1 ) );
     }
 
     /**
     * An action that takes more control of the results, in this case the path
-    * see ezpedia page on 'module' for a list of valid members for return array
+    * (see ezpedia page on 'module' for a list of valid members for return array).
+	* It also uses func_get_args to make the list of parameters/arguments even more dynamic
     */
     function action2()
     {
+        $arg_list = func_get_args();
         return array(
-            'content' => 'action2 has executed',
+            'content' => 'action2 was executed. ' . count( $arg_list ) . ' parameters received: '. htmlspecialchars( print_r( $arg_list, true ) ),
             'path' => array( array( 'url' => false, 'text' => 'hide controller name from path...' ) )
         );
     }
 
+    /**
+    * An action that takes more control of the results, in this case the pagelayout.
+    */
     function action3()
     {
         return array(

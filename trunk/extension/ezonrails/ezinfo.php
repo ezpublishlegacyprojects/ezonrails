d4470cbb3ebfde05dcec3dd30cf857fc98d84f6c
Gaetano Giunta <gg@ez.no>
2011-10-30 23:24:05 +0000

    - tag for next dev version - add changelog
    git-svn-id: http://svn.projects.ez.no/ezonrails@26 2b132ba1-f2ec-4eb1-8ca4-634b1ce61bd4

--- trunk/extension/ezonrails/ezinfo.php
+++ trunk/extension/ezonrails/ezinfo.php
@@ -5,7 +5,7 @@ class ezonrailsInfo
     static function info()
     {
         return array( 'Name' => 'eZ On Rails extension',
-                      'Version' => '0.2,
+                      'Version' => '0.3-dev,
                       'Copyright' => 'Copyright (C) 2010-2011 G. Giunta',
                       'License' => 'GNU General Public License v2.0'
                      );
